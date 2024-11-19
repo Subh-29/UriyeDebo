dca6c652b14e2b976663eddeaf8211eab97fff8b
Subh-29 <subhradip101@gmail.com>
2024-11-19 12:13:29 +0530

    Another one

--- chkRec.c
+++ chkRec.c
@@ -1,44 +1,67 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
 
-int main() {
-    int a[100], b[100], sum[100], wsum[100], chksum[100], carry = 0, n = 0;
-    printf("Enter number of bits in each segment: ");
-    scanf("%d", &n);
-    printf("For 1st seg enter:\n");
-    for(int i = 0; i < n; ++i) {
+void main() {
+    int a[10], b[10], c[20], sum1[10], carry = 0, n, i, p, chkr[20], temp;
+
+    printf("\n\r At the RECEIVER's end....");
+    carry = 0;
+
+    printf("\n\r Enter the received bits one after another:\n\r ");
+    for (i = n - 1; i >= 0; i--) {
         scanf("%d", &a[i]);
     }
-    printf("For 2nd seg enter:\n");
-    for(int i = 0; i < n; ++i) {
+
+    printf("\n\r");
+    for (i = n - 1; i >= 0; i--) {
         scanf("%d", &b[i]);
     }
 
-    for(int i = 0; i < n; ++i) {
-        sum[i] = (a[i] + b[i] + carry) % 2;
-        carry = (a[i] + b[i] + carry) / 2;
-    }
-    int p = 0;
-    for(int i = 0; i < n; ++i) {
-        p = (sum[i] + carry) % 2;
-        carry = (sum[i] + carry) / 2;
-        sum[i] = p;
+    printf("\n\rNext bits will be identified as checksum\n\r");
+    for (i = n - 1; i >= 0; i--) {
+        scanf("%d", &c[i]);
     }
 
-    for(int i = 0; i < n; ++i) {
-        wsum[i] = sum[i];
-    }
-    printf("\nWrap sum: ");
-    for(int i = 0; i < n; ++i) {
-        printf("%d ", wsum[i]);
+    printf("\n\r---------- Calculating Receiver's Checksum----------\n\r");
+    for (i = 0; i < n; i++) {
+        sum1[i] = (a[i] + b[i] + c[i] + carry) % 2;
+        carry = (a[i] + b[i] + c[i] + carry) / 2;
     }
 
-    for(int i = 0; i < n; ++i) {
-        chksum[i] = (wsum[i] == 1) ? 0 : 1;
+    printf("\n sum ....");
+    for (i = n - 1; i >= 0; i--) {
+        printf(" %d", sum1[i]);
     }
 
-    printf("\nCheck sum: ");
-    for(int i = 0; i < n; ++i) {
-        printf("%d ", chksum[i]);
+    if (carry == 1) {
+        for (i = 0; i < n; i++) {
+            p = (sum1[i] + carry) % 2;
+            carry = (sum1[i] + carry) / 2;
+            sum1[i] = p;
+        }
+    }
+
+    printf("\n Wrapped sum ....");
+    for (i = n - 1; i >= 0; i--) {
+        chkr[i] = sum1[i];
+        printf(" %d", sum1[i]);
+    }
+
+    printf("\n Checksum.....");
+    temp = 0;
+    for (i = n - 1; i >= 0; i--) {
+        if (chkr[i] == 1) {
+            chkr[i] = 0;
+        } else {
+            chkr[i] = 1;
+        }
+        temp = temp + chkr[i];
+        printf(" %d", chkr[i]);
+    }
+
+    if (temp != 0) {
+        printf("\n\r Checksum FAILED, Received data is Corrupted.");
+    } else {
+        printf("\n\r Checksum PASSED.");
     }
 }
---
+++ chkSend.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int a[100], b[100], sum[100], wsum[100], chksum[100], carry = 0, n = 0;
+    printf("Enter number of bits in each segment: ");
+    scanf("%d", &n);
+    printf("For 1st seg enter:\n");
+    for(int i = 0; i < n; ++i) {
+        scanf("%d", &a[i]);
+    }
+    printf("For 2nd seg enter:\n");
+    for(int i = 0; i < n; ++i) {
+        scanf("%d", &b[i]);
+    }
+
+    for(int i = 0; i < n; ++i) {
+        sum[i] = (a[i] + b[i] + carry) % 2;
+        carry = (a[i] + b[i] + carry) / 2;
+    }
+    int p = 0;
+    for(int i = 0; i < n; ++i) {
+        p = (sum[i] + carry) % 2;
+        carry = (sum[i] + carry) / 2;
+        sum[i] = p;
+    }
+
+    for(int i = 0; i < n; ++i) {
+        wsum[i] = sum[i];
+    }
+    printf("\nWrap sum: ");
+    for(int i = 0; i < n; ++i) {
+        printf("%d ", wsum[i]);
+    }
+
+    for(int i = 0; i < n; ++i) {
+        chksum[i] = (wsum[i] == 1) ? 0 : 1;
+    }
+
+    printf("\nCheck sum: ");
+    for(int i = 0; i < n; ++i) {
+        printf("%d ", chksum[i]);
+    }
+}
